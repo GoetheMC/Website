70655712056fb339157971c78bdcab4e6f793bb0
Zacklack <contact@zacklack.de>
2024-11-26 16:26:51 +0100

    Add SEO and Open Graph meta tags to improve site visibility

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,29 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="icon" href="public\favicon.ico" />
+        <title>GoetheMC - Minecraft Server</title>
+        <meta name="description" content="Trete noch heute dem GoetheMC bei!" />
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="GoetheMC - Minecraft Server" />
+        <meta
+          property="og:description"
+          content="Trete noch heute dem GoetheMC bei!"
+        />
+        <meta property="og:image" content="https://i.imgur.com/hZxR842.png" />
+        <meta property="og:url" content="https://goethemc.zacklack.de" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="GoetheMC - Minecraft Server" />
+        <meta
+          name="twitter:description"
+          content="Trete noch heute dem GoetheMC bei!"
+        />
+        <meta name="twitter:image" content="https://i.imgur.com/hZxR842.png" />
+        <meta name="twitter:url" content="https://goethemc.zacklack.de" />
       </Head>
       <main className="min-h-screen bg-gradient-to-b from-background to-accent">
         <Navbar />
